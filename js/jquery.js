6e940eabfbb6851d456f2d4cb8fd5f1fd8f277c8
YoChen <a0922335447@gmail.com>
2020-06-30 15:50:30 +0800

    yo02741
    WebDesign_index

---
+++ js/jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $('body').fullpage({
+        scrollBar: true,
+        navigation: false,
+        sectionSelector: 'section',
+        scrollingSpeed: 500,
+    });
+});
